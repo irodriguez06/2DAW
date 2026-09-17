d0f7ac9afa8fda96fefe94e69c4d424a7e0eea5e
Isaac <isaacrbx25@gmail.com>
2026-09-17 18:54:07 +0200

    Update spotifyLogin.ts
    Afegida interface Usuari i validació d'autentificació amb casos correcte i incorrecte

--- TEORIA/DWEC/Practica1_Spotify/spotifyLogin.ts
+++ TEORIA/DWEC/Practica1_Spotify/spotifyLogin.ts
@@ -1,11 +1,42 @@
-function autentificació (usuari: string, password: string, edat: number): 
-    boolean {
-        if (usuari == "admin" && password == "1234" && edat > 14) {
-            return true;
-    }
-        return false;
+interface Usuari {
+    correu: string;
+    password: string;
+    edat: number;
 }
 
-let resultat: boolean = autentificació("admin", "1234", 18);
+function autentificacio(usuari: Usuari): boolean {
+    let correcte: boolean = false;
 
-console.log(`${resultat}`);
+    if (usuari.correu === "admin@gmail.com" && usuari.password === "1234" && usuari.edat > 14) {
+        correcte = true;
+    }
+
+    return correcte;
+}
+
+function imprimir(auth: boolean): void {
+    if (auth) {
+        console.log("Usuari autentificat de forma correcte");
+    }
+    else {
+        console.log("Usuari, password o edat no vàlids");
+    }
+}
+
+const usuariCorrecte: Usuari = {
+    correu: "admin@gmail.com",
+    password: "1234",
+    edat: 18
+}
+
+let autOk: boolean = autentificacio(usuariCorrecte);
+imprimir(autOk);
+
+const usuariIncorrecte: Usuari = {
+    correu: "admin@gmail.com",
+    password: "1234",
+    edat: 12
+}
+
+let autOkIncorrecte: boolean = autentificacio(usuariIncorrecte);
+imprimir(autOkIncorrecte);
